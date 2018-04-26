7ceb982e1afb4b63fd1f271145bec409c3bd096f
TerryMc21 <G00340613@gmit.ie>
2018-04-26 15:45:58 +0100

    conversion objects added

--- G00340613/js/convert.js
+++ G00340613/js/convert.js
@@ -1,10 +1,39 @@
 function fToM(){
 
   var measure = parseInt(document.getElementById("value1").value);
+  var measure = parseInt(document.getElementById("value1").value);
+  var measure = parseInt(document.getElementById("value1").value);
+  var measure = parseInt(document.getElementById("value1").value);
+
+
+ 
+
+
+
 
   var meters = measure = 0.3048;
   var message = measure + ' feet converts to ' + meters + ' meters.';
   console.flag(message);
+  
+  var Centimeters = measure = 2.54;
+  var message = measure + ' centimeters converts to ' + inchs + ' inchs.';
+  console.flag(message);
+  
+  var yards = measure = 0.9144;
+  var message = measure + ' yards converts to ' + meters + ' meters.';
+  console.flag(message);
+  
+  var miles = measure = 1.60934;
+  var message = measure + ' miles converts to ' + miles + ' kilometers.';
+  console.flag(message);
+  
+  
+  
+  
+  
+  
   document.getElementbyId("resultsentence").innerHTML = message;
-
+  document.getElementbyId("resultsentence").innerHTML = message;
+  document.getElementbyId("resultsentence").innerHTML = message;
+  document.getElementbyId("resultsentence").innerHTML = message;
 }
